08345390c6014238633d253c04049ed930b661b6
thanhtai-personal <thanhtai.tttgalaxy@gmail.com>
2025-01-23 00:38:25 +0700

    Update HackerRoom.jsx

--- src/components/HackerRoom.jsx
+++ src/components/HackerRoom.jsx
@@ -14,26 +14,26 @@ export function HackerRoom(props) {
 
   return (
     <group {...props} dispose={null}>
-      <mesh geometry={nodes.screen_screens_0.geometry} material={materials.screens}>
+      <mesh geometry={nodes.screen_screens_0?.geometry} material={materials.screens}>
         <meshMatcapMaterial map={screenTxt} />
       </mesh>
-      <mesh geometry={nodes.screen_glass_glass_0.geometry} material={materials.glass} />
-      <mesh geometry={nodes.table_table_mat_0_1.geometry} material={materials.table_mat} />
-      <mesh geometry={nodes.table_table_mat_0_2.geometry} material={materials.computer_mat}>
+      <mesh geometry={nodes.screen_glass_glass_0?.geometry} material={materials.glass} />
+      <mesh geometry={nodes.table_table_mat_0_1?.geometry} material={materials.table_mat} />
+      <mesh geometry={nodes.table_table_mat_0_2?.geometry} material={materials.computer_mat}>
         <meshMatcapMaterial map={monitortxt} />
       </mesh>
-      <mesh geometry={nodes.table_table_mat_0_3.geometry} material={materials.server_mat} />
-      <mesh geometry={nodes.table_table_mat_0_4.geometry} material={materials.vhsPlayer_mat} />
-      <mesh geometry={nodes.table_table_mat_0_5.geometry} material={materials.stand_mat} />
-      <mesh geometry={nodes.table_table_mat_0_6.geometry} material={materials.mat_mat} />
-      <mesh geometry={nodes.table_table_mat_0_7.geometry} material={materials.arm_mat} />
-      <mesh geometry={nodes.table_table_mat_0_8.geometry} material={materials.tv_mat}>
+      <mesh geometry={nodes.table_table_mat_0_3?.geometry} material={materials.server_mat} />
+      <mesh geometry={nodes.table_table_mat_0_4?.geometry} material={materials.vhsPlayer_mat} />
+      <mesh geometry={nodes.table_table_mat_0_5?.geometry} material={materials.stand_mat} />
+      <mesh geometry={nodes.table_table_mat_0_6?.geometry} material={materials.mat_mat} />
+      <mesh geometry={nodes.table_table_mat_0_7?.geometry} material={materials.arm_mat} />
+      <mesh geometry={nodes.table_table_mat_0_8?.geometry} material={materials.tv_mat}>
         <meshMatcapMaterial map={monitortxt} />
       </mesh>
-      <mesh geometry={nodes.table_table_mat_0_9.geometry} material={materials.cables_mat} />
-      <mesh geometry={nodes.table_table_mat_0_10.geometry} material={materials.props_mat} />
-      <mesh geometry={nodes.table_table_mat_0_11.geometry} material={materials.ground_mat} />
-      <mesh geometry={nodes.table_table_mat_0_12.geometry} material={materials.key_mat} />
+      <mesh geometry={nodes.table_table_mat_0_9?.geometry} material={materials.cables_mat} />
+      <mesh geometry={nodes.table_table_mat_0_10?.geometry} material={materials.props_mat} />
+      <mesh geometry={nodes.table_table_mat_0_11?.geometry} material={materials.ground_mat} />
+      <mesh geometry={nodes.table_table_mat_0_12?.geometry} material={materials.key_mat} />
     </group>
   );
 }
